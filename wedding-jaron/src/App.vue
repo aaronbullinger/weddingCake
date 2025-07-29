48447b1a61a40edf1449b941de5287132c83030d
aaron <aaron.bullinger@gmail.com>
2025-07-29 18:16:38 +0200

    creating vue.js project with initial website

---
+++ wedding-jaron/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Willkommen auf unserer Hochzeits Homepage</h1>
+    </header>
+    <main>
+      <p>Hier entsteht Liebe!</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
