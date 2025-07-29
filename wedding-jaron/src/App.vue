43b2a73ddd7bef5cd35c1e752b09d76c1bad1773
aaron <aaron.bullinger@gmail.com>
2025-07-30 00:49:26 +0200

    refactor App.vue to use NavBar component and restructure routing

--- wedding-jaron/src/App.vue
+++ wedding-jaron/src/App.vue
@@ -25,4 +25,18 @@ export default {
 div {
   margin: 0.5rem;
 }
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px 10px 0;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+main {
+  margin-top: 80px;
+  padding: 0px 50px 0 50px;
+}
 </style>
