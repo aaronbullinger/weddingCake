8b28438a7d8a8f48ea600c5c4df036eac644e4fb
aaron <aaron.bullinger@gmail.com>
2025-07-30 00:51:57 +0200

    add first content to Rueckmeldung.vue

--- wedding-jaron/src/views/Rueckmeldung.vue
+++ wedding-jaron/src/views/Rueckmeldung.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="app">
-    <h1>
-      hallo 123
-    </h1>
-  </div>
+    <main>
+      <div class="info-box">
+        <div class="info-text">
+          <h2>
+            <span class="highlight">Damit wir besser planen können</span><br>
+            gebt uns doch gerne Feedback
+          </h2>
+
+          <p>Wir freuen uns auf Euch!</p>
+        </div>
+      </div>
+    </main>
 </template>
 
 <script setup lang="ts">
@@ -11,5 +18,35 @@
 </script>
 
 <style scoped>
+.info-box {
+  display: flex;
+  align-items: flex-start;
+  justify-content: left;
+  gap: 60px;
+}
 
+.info-text {
+  flex: 2;
+  max-width: 600px;
+  text-align: left;
+}
+
+.info-text h2 {
+  font-size: 2rem;
+  margin-bottom: 20px;
+  line-height: 1.3;
+  font-weight: 400;
+}
+
+.info-text h2 .highlight {
+  color: #A8CBB7;
+  font-weight: bold;
+}
+
+.info-text p {
+  font-size: 1rem;
+  line-height: 1.6;
+  color: #333;
+  margin-bottom: 15px;
+}
 </style>
