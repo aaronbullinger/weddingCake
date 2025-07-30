8d995ed94d0d6e09f05a996fc737b9744d5d9ac4
aaron <aaron.bullinger@gmail.com>
2025-07-31 00:07:06 +0200

    feat: update route imports to use alias and add scroll behavior

--- wedding-jaron/src/router/index.ts
+++ wedding-jaron/src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Rueckmeldung from '../views/Rueckmeldung.vue'
-import Location from '../views/Location.vue'
-import Unterkunft from '../views/Unterkunft.vue'
-import Details from '../views/Details.vue'
-import Faq from "../views/Faq.vue";
-import Home from "../views/Home.vue";
+import Rueckmeldung from '@/views/Rueckmeldung.vue'
+import Location from '@/views/Location.vue'
+import Unterkunft from '@/views/Unterkunft.vue'
+import Details from '@/views/Details.vue'
+import Faq from "@/views/Faq.vue";
+import Home from "@/views/Home.vue";
 
 const routes = [
     { path: '/', redirect: '/home' },
@@ -20,6 +20,9 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior() {
+        return { top: 0 };
+    }
 })
 
 export default router
